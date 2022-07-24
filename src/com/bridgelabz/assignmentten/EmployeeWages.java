ad5414492992fcef8e0d6ca664f0f5af9602ac2e
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-24 21:43:03 +0530

    employee wages of multiple companies managed using array

--- src/com/bridgelabz/assignmentten/EmployeeWages.java
+++ src/com/bridgelabz/assignmentten/EmployeeWages.java
@@ -1,8 +1,72 @@
 package com.bridgelabz.assignmentten;
 
 public class EmployeeWages {
+
     public static void main(String[] args)
     {
         System.out.println("Welcome to employee wage computation program");
     }
+
+    final int PRESENT = 1;
+    final int PART_TIME = 2;
+    String companyName;
+    int fullTimeWorkingHour, wagePerHour, maxWorkingDay, maxWorkingHour;
+    double totalSalary;
+
+
+    public EmployeeWages(String companyName, int fullTimeWorkingHour, int wagePerHour, int maxWorkingDay, int maxWorkingHour)
+    {
+        this.companyName = companyName;
+        this.fullTimeWorkingHour = fullTimeWorkingHour;
+        this.wagePerHour = wagePerHour;
+        this.maxWorkingDay = maxWorkingDay;
+        this.maxWorkingHour = maxWorkingHour;
+    }
+    public int checkEmpAttendance(int totalWorkingHour, int fullTimeWorkingHour, int maxWorkingHour)
+    {
+        if (totalWorkingHour == maxWorkingHour - (fullTimeWorkingHour / 2)) {
+            return PART_TIME;
+        }
+        else
+        {
+            return (int) (Math.random() * 3);
+        }
+    }
+
+    public int getWorkingHour(int isEmpPresent, int fullTimeWorkingHour)
+    {
+        switch (isEmpPresent)
+        {
+            case PRESENT:
+                return fullTimeWorkingHour;
+
+            case PART_TIME:
+                return fullTimeWorkingHour / 2;
+
+            default:
+                return 0;
+        }
+    }
+
+    public void calculateEmployeeWage()
+    {
+        int totalWorkingHour = 0;
+        int day = 0;
+        while (day < maxWorkingDay && totalWorkingHour < maxWorkingHour)
+        {
+            day++;
+            int isEmpPresent = checkEmpAttendance(totalWorkingHour, fullTimeWorkingHour, maxWorkingHour);
+
+            int empHr = getWorkingHour(isEmpPresent, fullTimeWorkingHour);
+
+            totalWorkingHour = totalWorkingHour + empHr;
+        }
+        totalSalary = totalWorkingHour * wagePerHour;
+    }
+    @Override
+    public String toString() {
+        return companyName + " Employee monthly salary is = $" + totalSalary + " USD";
+    }
+
+
 }
---
+++ src/com/bridgelabz/assignmentten/EmployeeWagesTest.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.assignmentten;
+
+public class EmployeeWagesTest {
+    EmployeeWages[] companies;
+    int lastIndex;
+
+    public EmployeeWagesTest() {
+        companies = new EmployeeWages[100];
+    }
+
+    public void addCompany(String companyName, int fullTimeWorkingHour, int wagePerHour,
+                           int maxWorkingDay, int maxWorkingHour) {
+        EmployeeWages employeeWages = new EmployeeWages(companyName,
+                fullTimeWorkingHour, wagePerHour, maxWorkingDay, maxWorkingHour);
+        employeeWages.calculateEmployeeWage();
+        companies[lastIndex] = employeeWages;
+        lastIndex++;
+    }
+
+    public void printAllCompanies() {
+        for (int i = 0; i < companies.length; i++) {
+            if (companies[i] != null) {
+                System.out.println(companies[i]);
+                System.out.println("<------------------------------------------------------------------------>");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EmployeeWagesTest employeeWagesTest = new EmployeeWagesTest();
+
+        employeeWagesTest.addCompany("Tcs", 8, 20, 20, 60);
+        employeeWagesTest.addCompany("Wipro", 9, 22, 22, 80);
+        employeeWagesTest.addCompany("Google", 10, 26, 20, 80);
+        employeeWagesTest.addCompany("Microsoft", 8, 20, 25, 60);
+        employeeWagesTest.addCompany("Hcl", 9, 22, 25, 100);
+        employeeWagesTest.addCompany("Dell", 10, 26, 20, 90);
+        employeeWagesTest.printAllCompanies();
+    }
+}
